06f8bd7ad8b0c11f11ef0378026bbeffc65aa615
Benjamin Correa <control.benjamin@gmail.com>
2020-08-20 21:23:33 -0300

    primer publicacion en azure

---
+++ VideoClubCore/Controllers/ClientesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VideoClubCore.Data;
+using VideoClubCore.Models;
+
+
+namespace VideoClubCore.Controllers
+{
+    [AllowAnonymous]
+    public class ClientesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public IActionResult Index()
+        {
+
+            var viewModel = new ClientesViewModel
+            {
+                Clientes = _context.Clientes.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
--- VideoClubCore/Controllers/HomeController.cs
+++ VideoClubCore/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using VideoClubCore.Data;
 using VideoClubCore.Models;
 
 namespace VideoClubCore.Controllers
@@ -12,14 +13,30 @@ namespace VideoClubCore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext   dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
         {
+
+            var valor = _dbContext.Clientes.Where(c => c.Id == 5)
+                .Select(c =>c.Nombres)
+                .FirstOrDefault();
+
+    
+
+            ViewBag.nombre = valor.ToString();
+
+            var model = new ClientesViewModel() {
+                Clientes = _dbContext.Clientes.ToList()
+            };
+
+            ViewBag.lista = model.Clientes;
             return View();
         }
 
